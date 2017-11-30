15a199cfd87946c92fd23a44d16380aa6fe3862b
Ekaterina Danilova <edanilova@axcient.com>
2017-11-30 21:48:02 +0300

    Login to admin

---
+++ login_to_admin.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'selenium-webdriver'
+require 'pp'
+
+$browser = 'firefox'
+
+describe 'Login to admin' do
+  before(:each) do
+    @driver = Selenium::WebDriver.for :firefox
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  end
+
+  it 'should login to admin' do
+    @driver.navigate.to 'http://localhost/litecart/admin'
+    @driver.find_element(:name, 'username').send_keys 'admin'
+    @driver.find_element(:name, 'password').send_keys 'admin'
+    @driver.find_element(:name, 'login').click
+    @wait.until { @driver.title == 'My Store'}
+  end
+
+  after(:each) do
+    @driver.quit
+  end
+end
